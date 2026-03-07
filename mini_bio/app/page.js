35b54be7a840d79901f48e179afb99064cd52a7f
Anna Beatriz <anna.00000851757@unicap.br>
2026-03-07 00:22:13 +0000

    bio

--- mini_bio/app/page.js
+++ mini_bio/app/page.js
@@ -1,64 +1,46 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+    <div className="flex min-h-screen items-center justify-center bg-[#E2D5F1]">
+      <main className="bg-[#f3ecfa] p-10 rounded-xl shadow-md text-center max-w-md">
+
+        <h1 className="text-purple-800 text-3xl font-bold mb-4">
+          Anna Beatriz
+        </h1>
+
+        <p className="text-gray-600 mb-4">
+          Como visto acima, me chamo Anna Beatriz! Atualmente curso Ciência da Computação, tenho 21 anos e venho gostando bastante do curso,
+          apesar de às vezes me perguntar se estou na área certa (o que acontece bastante em semanas de prova).
+        </p>
+
+        <p className="text-gray-600 mb-6">
+          Brincadeiras à parte, venho me interessando cada vez mais pela área e tentando descobrir com qual ramo da computação me identifico 
+          mais, para que eu possa direcionar melhor meus estudos no futuro.
+        </p>
+
+        <p className="text-gray-600 mb-8">
+          Além da computação, também gosto de assistir filmes e séries, ler livros e praticar esportes. No tempo livre, atividades artesanais, 
+          como crochê, desenho e pintura. Valorizo tempo de qualidade com minha família e com as pessoas que são importantes para mim.
+        </p>
+
+
+        <div className="flex gap-4 justify-center">
           <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            className="text-purple-700 hover:underline"
+            href="https://github.com/Anninhaxs"
             target="_blank"
-            rel="noopener noreferrer"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
+            GitHub
           </a>
+
           <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            className="text-purple-700 hover:underline"
+            href="https://www.linkedin.com/in/annaxs"
             target="_blank"
-            rel="noopener noreferrer"
           >
-            Documentation
+            LinkedIn
           </a>
         </div>
+
       </main>
     </div>
   );
